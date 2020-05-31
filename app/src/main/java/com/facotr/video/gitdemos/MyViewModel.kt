4fb17bb9138bacbf5b26c958d7a7ae4d9c7ec5b0
“Sunday” <510131752@qq.com>
2020-05-31 21:18:00 +0800

    refactor: use LiveData in MyViewModel

--- app/src/main/java/com/facotr/video/gitdemos/MyViewModel.kt
+++ app/src/main/java/com/facotr/video/gitdemos/MyViewModel.kt
@@ -1,9 +1,20 @@
 package com.facotr.video.gitdemos
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
 
     var number = 0
+    private var _number = MutableLiveData(0)
+    var numerLiveData = _number
+
+    fun add(n: Int){
+        _number.value = _number.value?.plus(n)
+    }
+
+    fun reset(){
+        _number.value = 0
+    }
 
 }
