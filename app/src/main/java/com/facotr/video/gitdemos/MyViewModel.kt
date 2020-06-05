317d84276accd0aecea18ce4e2af470b1d02c6ce
“lixin” <510131752@qq.com>
2020-06-05 08:07:37 +0800

    clean: 冲突测试啦4

--- app/src/main/java/com/facotr/video/gitdemos/MyViewModel.kt
+++ app/src/main/java/com/facotr/video/gitdemos/MyViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
 
+    private var names = "sunday"
     private var _numbers = MutableLiveData(0)
+    private var users = "sundayss"
     var numerLiveData = _numbers
 
     fun add(n: Int){
