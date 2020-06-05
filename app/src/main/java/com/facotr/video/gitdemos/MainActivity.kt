5e1a188529d475af1af509ecff0b36c11e33953f
“lixin” <510131752@qq.com>
2020-06-05 08:00:19 +0800

    clean: 冲突测试啦 嗷嗷喊

--- app/src/main/java/com/facotr/video/gitdemos/MainActivity.kt
+++ app/src/main/java/com/facotr/video/gitdemos/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
          }
 
          findViewById<Button>(R.id.button2).setOnClickListener {
-             viewModel.add(-1)
+             viewModel.add(-1) //冲突测试 啊哈哈
          }
 
     }
